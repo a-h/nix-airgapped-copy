4b594023caf60de951942e04ed6164cda1abfc44
Adrian Hesketh <adrianhesketh@hushmail.com>
2024-07-18 08:58:03 +0100

    feat: update to copy symlinks

--- import.sh
+++ import.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 nix copy --all --offline --impure  --no-check-sigs --from file://$PWD/nix-export/
 nix build --offline --impure
+cp -rL result/ tmp-result
+rm -rf result
+mv tmp-result result
