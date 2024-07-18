51619cc0f2fb13c05920ce5139068f24fc42d87e
Adrian Hesketh <adrianhesketh@hushmail.com>
2024-07-18 09:56:08 +0100

    docs: update docs

--- export.sh
+++ export.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+# Build everything you want to transfer across the airgap.
 nix develop
 nix build
 nix build .#docker-image
+# Copy the whole of the local store to the export directory.
+# The local store was populated by executing the builds.
 nix copy --derivation --all --impure --to file://$PWD/nix-export/
-grep StorePath nix-export/*.narinfo | awk '{print $2}' > nix-export.txt
-cat nix-export.txt | wc -l
+# Copy the flake archive to the export directory.
 nix flake archive --to file://$PWD/nix-export/
-grep StorePath nix-export/*.narinfo | awk '{print $2}' >> nix-export.txt
-cat nix-export.txt | wc -l
--- import.sh
+++ import.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# Restore the nix store into the airgapped machine.
 nix copy --all --offline --impure --no-check-sigs --from file://$PWD/nix-export/
+# Demonstrate that it's possible to build the output.
 nix build --offline --impure
+# The result directory contains symlinks, so they need to be flattened.
 cp -rL result/ tmp-result
 rm -rf result
 mv tmp-result result
