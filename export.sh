d066abc8a5d31d2bca8ffe012b9dd110d459b48d
Adrian Hesketh <adrianhesketh@hushmail.com>
2024-07-18 09:48:57 +0100

    ensure that the docker image gets built

--- export.sh
+++ export.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 nix develop
 nix build
+nix build .#docker-image
 nix copy --derivation --all --impure --to file://$PWD/nix-export/
 grep StorePath nix-export/*.narinfo | awk '{print $2}' > nix-export.txt
 cat nix-export.txt | wc -l
